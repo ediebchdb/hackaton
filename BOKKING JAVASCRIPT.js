1ff526aa46204807f0c0500196edcd410e682647
Manan Awasthi <152317038+ediebchdb@users.noreply.github.com>
2024-02-29 23:32:24 +0530

    Add files via upload

---
+++ BOKKING JAVASCRIPT.js
@@ -0,0 +1,5 @@
+document.getElementById("booking-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // You can add your booking logic here, such as sending data to a server or displaying a confirmation message
+    alert("Booking Successful!");
+});
